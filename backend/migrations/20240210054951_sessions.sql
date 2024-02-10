644dc71a6c211feab623eee1d04ebb9fa7ed7aa2
Gautham Krishna <gautham.jayasurya@gmail.com>
2024-02-10 14:00:16 +0530

    adding sessions table migration

--- backend/migrations/20240210054951_sessions.sql
+++ backend/migrations/20240210054951_sessions.sql
@@ -1,6 +1,6 @@
 --migration for sessions table
 CREATE TABLE IF NOT EXISTS "sessions"(
-    session_id uuid UNIQUE NOT NULL uuid_generate_v4(),
+    session_id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
     user_id uuid UNIQUE NOT NULL,
     expiry TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE
