5a59d2593198f63eb2ba8490918f595d75f55fd8
Gautham Krishna <gautham.jayasurya@gmail.com>
2024-03-25 13:32:14 +0530

    stock_validation sql function created

--- backend/migrations/20240321131219_functions.sql
+++ backend/migrations/20240321131219_functions.sql
@@ -2,7 +2,21 @@ CREATE OR REPLACE FUNCTION item_ownership(item_id UUID, user_id UUID) RETURNS BO
 DECLARE
     id_1 alias for $1;
     id_2 alias for $2;
+    result BOOLEAN;
 BEGIN
-    SELECT * FROM "item" WHERE "item"."item_id" = id_1 AND "item"."user_id" = id_2;
+    SELECT EXISTS (SELECT * FROM "item" WHERE "item"."item_id" = id_1 AND "item"."user_id" = id_2) INTO result ;
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION stock_validation(item_id UUID, quantity INT) RETURNS BOOLEAN AS $$
+DECLARE
+    id_1 alias for $1;
+    quantity_1 alias for $2;
+    result BOOLEAN;
+BEGIN
+    SELECT EXISTS (SELECT * FROM "stock" WHERE "stock"."item_id" = id_1 AND "stock"."quantity" >= quantity_1) INTO result ;
+    RETURN result;
 END;
 $$ LANGUAGE plpgsql;
