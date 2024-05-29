16526fb14ff58023abbfa2807f12b313578efc73
Gautham Krishna <guatham.jayasurya@gmail.com>
2024-05-29 23:45:19 +0530

    Add order and address tables to database schema

--- backend/migrations/20240529164652_orders_and_address.sql
+++ backend/migrations/20240529164652_orders_and_address.sql
@@ -1,9 +1,20 @@
 CREATE TABLE IF NOT EXISTS "order" (
-    order_id UUID PRIMARY KEY NOT NULL,
+    order_id UUID DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
-    order_date TIMESTAMP NOT NULL,
-    total_amount DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE
+    order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    dispatched BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (item_id) REFERENCES "item"(item_id),
+    PRIMARY KEY (order_id, user_id, item_id)
+);
+
+CREATE TABLE IF NOT EXISTS "order_items"(
+    order_id UUID NOT NULL,
+    item_id UUID NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES "order"(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES "item"(item_id) ON DELETE CASCADE,
+    PRIMARY KEY (order_id, item_id)
 );
 
 CREATE TABLE IF NOT EXISTS "address" (
