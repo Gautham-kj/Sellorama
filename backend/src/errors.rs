9ffb8996e7f49676073e3c11def83c9fd49d4360
Gautham Krishna <guatham.jayasurya@gmail.com>
2024-05-30 16:43:28 +0530

    chore: Add MyError enum and ErrorResponse struct

---
+++ backend/src/errors.rs
@@ -0,0 +1,42 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde::Serialize;
+use serde_json::json;
+use utoipa::{ToResponse, ToSchema};
+pub enum MyError {
+    InternalServerError,
+    ConflictError,
+    UnauthorizedError,
+    UnproccessableEntityError,
+    CustomError((u16, String)),
+}
+
+#[derive(Serialize, ToSchema, ToResponse)]
+pub struct ErrorResponse {
+    detail: String,
+}
+
+impl IntoResponse for MyError {
+    fn into_response(self) -> Response {
+        let (statuscode, body) = match self {
+            MyError::ConflictError => (StatusCode::CONFLICT, "Conflict Error".to_string()),
+            MyError::InternalServerError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal Server Error".to_string(),
+            ),
+            MyError::UnauthorizedError => (
+                StatusCode::UNAUTHORIZED,
+                "User Is Not Authorized".to_string(),
+            ),
+            MyError::UnproccessableEntityError => (
+                StatusCode::UNPROCESSABLE_ENTITY,
+                "Unproccessable Entity".to_string(),
+            ),
+            MyError::CustomError(code) => (StatusCode::from_u16(code.0).unwrap(), code.1),
+        };
+        (statuscode, Json(json!(ErrorResponse { detail: body }))).into_response()
+    }
+}
