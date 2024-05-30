710be578db9e579a40512c5ab3d889072db2873e
Gautham Krishna <guatham.jayasurya@gmail.com>
2024-05-30 17:38:36 +0530

    chore: Implement MyError enum and ErrorResponse struct

--- backend/src/cart.rs
+++ backend/src/cart.rs
@@ -1,3 +1,4 @@
+use crate::errors::MyError;
 use crate::user::{check_session_validity, extract_session_header, GeneralResponse};
 use crate::AppState;
 use axum::{
@@ -44,19 +45,11 @@ pub struct CartResponse {
 /// Get Cart
 ///
 /// Endpoint to get all items in a user's cart
-pub async fn get_cart(state: State<AppState>, headers: HeaderMap) -> impl IntoResponse {
-    let session_id;
-    match extract_session_header(headers).await {
-        Some(session) => session_id = session,
-        None => {
-            return (
-                StatusCode::UNAUTHORIZED,
-                Json(json!(GeneralResponse {
-                    detail: "Invalid Credentials".to_string()
-                })),
-            )
-        }
-    }
+pub async fn get_cart(
+    state: State<AppState>,
+    headers: HeaderMap,
+) -> Result<impl IntoResponse, MyError> {
+    let session_id = extract_session_header(headers).await?;
     match check_session_validity(&state.db_pool, session_id).await {
         Some(user) => {
             let query = r#"SELECT "item_id","quantity" FROM "cart" WHERE "cart_id" = $1"#;
@@ -66,26 +59,16 @@ pub async fn get_cart(state: State<AppState>, headers: HeaderMap) -> impl IntoRe
                 .fetch_all(&state.db_pool)
                 .await
             {
-                Ok(cart) => (
+                Ok(cart) => Ok((
                     StatusCode::OK,
                     Json(json!(CartResponse {
                         detail: Cart { items: cart }
                     })),
-                ),
-                Err(_e) => (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    Json(json!(GeneralResponse {
-                        detail: "Internal Server Error".to_string()
-                    })),
-                ),
+                )),
+                Err(_e) => Err(MyError::InternalServerError),
             }
         }
-        None => (
-            StatusCode::UNAUTHORIZED,
-            Json(json!(GeneralResponse {
-                detail: "User not authorized".to_string()
-            })),
-        ),
+        None => Err(MyError::UnauthorizedError),
     }
 }
 
@@ -104,19 +87,8 @@ pub async fn add_item(
     headers: HeaderMap,
     state: State<AppState>,
     Form(form_data): Form<CartItem>,
-) -> impl IntoResponse {
-    let session_id;
-    match extract_session_header(headers).await {
-        Some(session) => session_id = session,
-        None => {
-            return (
-                StatusCode::UNAUTHORIZED,
-                Json(json!(GeneralResponse {
-                    detail: "Invalid Credentials".to_string()
-                })),
-            )
-        }
-    }
+) -> Result<impl IntoResponse, MyError> {
+    let session_id = extract_session_header(headers).await?;
     match check_session_validity(&state.db_pool, session_id).await {
         Some(userresponse) => {
             //create plsql function to check and return stock issues
@@ -132,33 +104,18 @@ pub async fn add_item(
                 .await
             {
                 Ok(response) => match response {
-                    Some(_t) => (
+                    Some(_t) => Ok((
                         StatusCode::OK,
                         Json(json!(GeneralResponse {
                             detail: "Item Added to cart".to_string()
                         })),
-                    ),
-                    None => (
-                        StatusCode::CONFLICT,
-                        Json(json!(GeneralResponse {
-                            detail: "Not added to cart".to_string()
-                        })),
-                    ),
+                    )),
+                    None => Err(MyError::ConflictError),
                 },
-                Err(e) => (
-                    StatusCode::OK,
-                    Json(json!(GeneralResponse {
-                        detail: e.to_string()
-                    })),
-                ),
+                Err(_e) => Err(MyError::InternalServerError),
             }
         }
-        None => (
-            StatusCode::UNAUTHORIZED,
-            Json(json!(GeneralResponse {
-                detail: "Invalid Credentials".to_string()
-            })),
-        ),
+        None => Err(MyError::UnauthorizedError),
     }
 }
 #[utoipa::path(
@@ -177,19 +134,8 @@ pub async fn update_cart_item(
     headers: HeaderMap,
     state: State<AppState>,
     Form(form_data): Form<CartItem>,
-) -> impl IntoResponse {
-    let session_id;
-    match extract_session_header(headers).await {
-        Some(session) => session_id = session,
-        None => {
-            return (
-                StatusCode::UNAUTHORIZED,
-                Json(json!(GeneralResponse {
-                    detail: "Invalid Credentials".to_string()
-                })),
-            )
-        }
-    }
+) -> Result<impl IntoResponse, MyError> {
+    let session_id = extract_session_header(headers).await?;
     match check_session_validity(&state.db_pool, session_id).await {
         Some(userresponse) => {
             if form_data.quantity > 0 {
@@ -202,25 +148,20 @@ pub async fn update_cart_item(
                     .await
                 {
                     Ok(response) => match response {
-                        Some(_t) => (
+                        Some(_t) => Ok((
                             StatusCode::OK,
                             Json(json!(GeneralResponse {
                                 detail: "Cart updated".to_string()
                             })),
-                        ),
-                        None => (
+                        )),
+                        None => Ok((
                             StatusCode::OK,
                             Json(json!(GeneralResponse {
                                 detail: "Cart Not Updated".to_string()
                             })),
-                        ),
+                        )),
                     },
-                    Err(_e) => (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!(GeneralResponse {
-                            detail: "Server Error".to_string()
-                        })),
-                    ),
+                    Err(_e) => Err(MyError::InternalServerError),
                 }
             } else {
                 match sqlx::query!(
@@ -231,27 +172,17 @@ pub async fn update_cart_item(
                 .execute(&state.db_pool)
                 .await
                 {
-                    Ok(_response) => (
+                    Ok(_response) => Ok((
                         StatusCode::OK,
                         Json(json!(GeneralResponse {
                             detail: "Cart updated".to_string()
                         })),
-                    ),
-                    Err(_e) => (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!(GeneralResponse {
-                            detail: "Server Error".to_string()
-                        })),
-                    ),
+                    )),
+                    Err(_e) => Err(MyError::InternalServerError),
                 }
             }
         }
-        None => (
-            StatusCode::UNAUTHORIZED,
-            Json(json!(GeneralResponse {
-                detail: "Invalid Credentials".to_string()
-            })),
-        ),
+        None => Err(MyError::UnauthorizedError),
     }
 }
 #[utoipa::path(
@@ -268,19 +199,11 @@ pub async fn update_cart_item(
 /// SubCheckout Cart
 ///
 /// Checking whether items in the cart are still in stock.
-pub async fn check_cart(headers: HeaderMap, state: State<AppState>) -> impl IntoResponse {
-    let session_id;
-    match extract_session_header(headers).await {
-        Some(session) => session_id = session,
-        None => {
-            return (
-                StatusCode::UNAUTHORIZED,
-                Json(json!(GeneralResponse {
-                    detail: "Invalid Credentials".to_string()
-                })),
-            )
-        }
-    }
+pub async fn check_cart(
+    headers: HeaderMap,
+    state: State<AppState>,
+) -> Result<impl IntoResponse, MyError> {
+    let session_id = extract_session_header(headers).await?;
     match check_session_validity(&state.db_pool, session_id).await {
         Some(user) => {
             let query = r#"DELETE FROM "cart" 
@@ -293,39 +216,22 @@ pub async fn check_cart(headers: HeaderMap, state: State<AppState>) -> impl Into
                 .bind(user.user_id)
                 .fetch_all(&state.db_pool)
                 .await
-                .map_err(|err| {
-                    println!("{}", err);
-                    return (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!(GeneralResponse {
-                            detail: "Server Error".to_string()
-                        })),
-                    );
-                }) {
+                .map_err(|_| MyError::InternalServerError)
+            {
                 Ok(items) => match items.len() {
-                    0 => (
+                    0 => Ok((
                         StatusCode::OK,
                         Json(json!(GeneralResponse {
                             detail: "Items In Stock, Proceed to Checkout".to_string()
                         })),
-                    ),
-                    _ => (
-                        StatusCode::CONFLICT,
-                        Json(json!(CartResponse {
-                            detail: Cart { items: items }
-                        })),
-                    ),
+                    )),
+                    _ => Err(MyError::ConflictError),
                 },
-                Err(e) => e,
+                Err(e) => Err(e),
             }
         }
-        None => (
-            StatusCode::UNAUTHORIZED,
-            Json(json!(GeneralResponse {
-                detail: "Invalid Credentials".to_string()
-            })),
-        ),
+        None => Err(MyError::UnauthorizedError),
     }
 }
 
-
+// pub async fn create_order(headers: HeaderMap, state: State<AppState>) -> impl IntoResponse {}
